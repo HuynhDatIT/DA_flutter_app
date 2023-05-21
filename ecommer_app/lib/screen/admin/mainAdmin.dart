962b2fa7129de97c2ea11bba912e45d233377ae1
DESKTOP-RDGMCAA\DAT <datht38@fpt.com.vn>
2023-05-21 11:12:39 +0700

    intit admin page

---
+++ ecommer_app/lib/screen/admin/mainAdmin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainAdmin extends StatefulWidget {
+  const MainAdmin({ Key? key }) : super(key: key);
+
+  @override
+  _MainAdminState createState() => _MainAdminState();
+}
+
+class _MainAdminState extends State<MainAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
