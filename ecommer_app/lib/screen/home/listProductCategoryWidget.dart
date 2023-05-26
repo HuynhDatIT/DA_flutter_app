b0cb032b2b41b3305b97ab02eeff769b54c07026
DESKTOP-RDGMCAA\DAT <datht38@fpt.com.vn>
2023-05-26 21:23:39 +0700

    add list product

--- ecommer_app/lib/screen/home/listProductCategoryWidget.dart
+++ ecommer_app/lib/screen/home/listProductCategoryWidget.dart
@@ -15,15 +15,44 @@ class _ListProductCategoryWidgetState extends State<ListProductCategoryWidget> {
   @override
   Widget build(BuildContext context) {
     List<ProductItemWidget> listProductWidget = widget.listProduct
-        .take(12)
+        .take(2)
         .map(
           (e) => ProductItemWidget(product: e),
         )
         .toList();
-    List<List<ProductItemWidget>> groupListProductWidget;
+    //List<List<ProductItemWidget>> groupListProductWidget;
 
     return Column(
-      children: [Container(), Container()],
+      children: [
+        Container(
+          padding: const EdgeInsets.only(bottom: 10),
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(color: Colors.black),
+        ),
+        Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Container(
+                        height: MediaQuery.of(context).size.width / 2.5,
+                        child: Row(children: listProductWidget))),
+                Expanded(
+                    child: Container(
+                        height: MediaQuery.of(context).size.width / 2.5,
+                        child: Row(children: listProductWidget))),
+              ],
+            )),
+        const Padding(
+          padding: EdgeInsets.only(top: 20, bottom: 20),
+          child: Divider(
+            height: 1,
+            color: Color.fromARGB(255, 146, 143, 143),
+          ),
+        )
+      ],
     );
   }
 }
--- ecommer_app/lib/screen/home/mainHomeScreen.dart
+++ ecommer_app/lib/screen/home/mainHomeScreen.dart
@@ -1,4 +1,4 @@
-
+import 'package:ecommer_app/screen/home/listProductCategoryWidget.dart';
 import 'package:ecommer_app/screen/home/listProductWidget.dart';
 import 'package:ecommer_app/screen/home/topwidget.dart';
 import 'package:flutter/material.dart';
@@ -6,25 +6,31 @@ import 'package:flutter/material.dart';
 import '../../data/mockData.dart';
 
 class MainHomeScreen extends StatelessWidget {
- MainHomeScreen({ Key? key }) : super(key: key);
+  MainHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            SizedBox(
-                height: MediaQuery.of(context).size.height / 3,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Topwidget(listImage: MockData.listImage),
-                )),
-            ListProductWidget(listproduct: MockData.listProductSale, title: 'SẢN PHẨM GIẢM GIÁ'),
-            ListProductWidget(listproduct: MockData.listProductNew, title: 'SẢN PHẨM MỚI NHẤT'),
-            ListProductWidget(listproduct: MockData.listProduct, title: 'SẢN PHẨM DÀNH CHO BẠN'),
-          ],
-        ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          SizedBox(
+              height: MediaQuery.of(context).size.height / 3,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Topwidget(listImage: MockData.listImage),
+              )),
+          ListProductWidget(
+              listproduct: MockData.listProductSale,
+              title: 'SẢN PHẨM GIẢM GIÁ'),
+          ListProductWidget(
+              listproduct: MockData.listProductNew, title: 'SẢN PHẨM MỚI NHẤT'),
+          ListProductWidget(
+              listproduct: MockData.listProduct,
+              title: 'SẢN PHẨM DÀNH CHO BẠN'),
+          ListProductCategoryWidget(listProduct: MockData.listProduct)
+        ],
+      ),
     );
   }
 }
