6355befe5971732845626b33a661d076a97d1544
trquangtanh <tanhtran101@gmail.com>
2023-05-27 10:05:46 +0700

    mer admin

--- ecommer_app/lib/main.dart
+++ ecommer_app/lib/main.dart
@@ -32,6 +32,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: MainHomeScreen()));
+    return Scaffold(body: Center(child: MainAdmin()));
   }
 }
